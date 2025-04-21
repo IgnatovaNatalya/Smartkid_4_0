baabe04b6746ffb3a27bfe834d33fc95c1a00cb6
IgnatovaNatalya <ananjeva@gmail.com>
2025-04-22 00:18:42 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "ru.mamsikgames.smartkid"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ru.mamsikgames.smartkid"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 4
+        versionName = "4.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+
+    implementation(libs.koin)
+
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    //ksp(libs.room.compiler)
+
+    implementation(libs.room.ktx)
+    implementation(libs.room.rxJava3)
+
+    //implementation(libs.rxjava3.rxandroid)
+    //implementation(libs.rxjava3.rxjava)
+
+}
